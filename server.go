446d682dc42cac7f3be25513f70e916b4e46c366
Marcus Cartágenes <mvcartagenes@corp.globo.com>
2018-10-02 17:21:34 -0300

    Firebase operations

--- server.go
+++ server.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"gopkg.in/zabawaba99/firego.v1"
 
 	"github.com/gorilla/mux"
 )
 
 type Status struct {
-	User string  `json:"user_id"`
-	Peso float32 `json:"peso"`
+	User        string `json:"user_id"`
+	Peso        string `json:"peso"`
+	Porcentagem int    `json:"porcentagem"`
 }
 
 func main() {
@@ -22,14 +24,30 @@ func main() {
 }
 
 func trataResposta(w http.ResponseWriter, r *http.Request) {
+	fireInstance := firego.New("https://gasnow-acf5c.firebaseio.com/", nil)
 	var status Status
 	body, _ := ioutil.ReadAll(r.Body)
 	err := json.Unmarshal([]byte(string(body)), &status)
-
 	if err != nil {
 		panic(err)
 	}
+	status.Porcentagem = calcularPorcentagem(status.Peso)
+	db, _ := fireInstance.Ref("/botijoes")
 
-	fmt.Println(status.Peso)
-	fmt.Println(status.User)
+	var result map[string]interface{} = make(map[string]interface{})
+
+	err = db.OrderBy("user_id").EqualTo("1").LimitToFirst(1).Value(&result)
+
+	for k, _ := range result {
+		db, _ = db.Ref("/botijoes/" + k)
+		status.Porcentagem = 100000
+		db.Update(status)
+	}
+
+}
+
+func calcularPorcentagem(peso string) int {
+	//CONVERTER O PESO PARA DOUBLE
+	//CALCULAR A PORCENTAGEM
+	return 0
 }
